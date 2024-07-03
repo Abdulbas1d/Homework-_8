b547c2bacf7c5dd8aec8205d06be8e5f1b83eb97
Abdulbas1d <muhammadaliyevabdulbosid@gmail.com>
2024-07-03 15:05:20 +0500

    The different codes are writed

---
+++ js/indx.js
@@ -0,0 +1,22 @@
+const form = document.getElementById("form");
+const button = document.getElementById("btn");
+const cards = document.getElementById("cards");
+const task = document.getElementById("task");
+const btn1 = document.getElementById("btn1");
+const btn2 =document.getElementById("btn2");
+
+function data() {
+    return `
+        <div class="card">
+            <h1 class="task_name">${task.value}</h1>
+            <button id="btn1" class="btn">Edit</button>
+            <button id="btn2"  class="btn1">Delete</button>
+        </div>
+    `
+}
+
+button && button.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    
+})
